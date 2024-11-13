deddb12e86db8d718d5cf902b2b37b0a43ae0b70
Tony <legendmastertony@gmail.com>
2024-11-13 13:34:49 +0800

    Fix mac compile

--- crates/tauri-runtime-wry/src/monitor/macos.rs
+++ crates/tauri-runtime-wry/src/monitor/macos.rs
@@ -6,11 +6,11 @@ impl super::MonitorExt for tao::monitor::MonitorHandle {
   fn get_work_area_size(&self) -> tao::dpi::PhysicalSize<u32> {
     use objc2_app_kit::NSScreen;
     use tao::platform::macos::MonitorHandleExtMacOS;
-    if let Some(ns_screen) = target_monitor.ns_screen() {
+    if let Some(ns_screen) = self.ns_screen() {
       let ns_screen: &NSScreen = unsafe { &*ns_screen.cast() };
       let rect = ns_screen.visibleFrame();
-      return TaoLogicalSize::new(rect.size.width, rect.size.height)
-        .to_physical(target_monitor.scale_factor());
+      return tao::dpi::LogicalSize::new(rect.size.width, rect.size.height)
+        .to_physical(self.scale_factor());
     }
     self.size()
   }
