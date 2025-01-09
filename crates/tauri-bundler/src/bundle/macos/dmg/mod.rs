aa9ad22eb23a3139c40f0ef30ebf9f8667b1b2c7
fabianlars <fabianlars@fabianlars.de>
2025-01-09 17:29:27 +0100

    fix

--- crates/tauri-bundler/src/bundle/macos/dmg/mod.rs
+++ crates/tauri-bundler/src/bundle/macos/dmg/mod.rs
@@ -194,10 +194,10 @@ pub fn bundle_project(settings: &Settings, bundles: &[Bundle]) -> crate::Result<
   fs::rename(bundle_dir.join(dmg_name), dmg_path.clone())?;
 
   // Sign DMG if needed
-
-  if let Some(keychain) = super::sign::keychain(settings.macos().signing_identity.as_deref())? {
-    // Skip self-signing DMGs https://github.com/tauri-apps/tauri/issues/12288
-    if keychain != "-" {
+  // skipping self-signing DMGs https://github.com/tauri-apps/tauri/issues/12288
+  let identity = settings.macos().signing_identity.as_deref();
+  if identity != Some("-") {
+    if let Some(keychain) = super::sign::keychain(identity)? {
       super::sign::sign(
         &keychain,
         vec![super::sign::SignTarget {
