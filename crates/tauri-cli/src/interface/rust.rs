3311b8991d0e26c94f27eb7c0e42d2ccd395de69
FabianLars <fabianlars@fabianlars.de>
2025-01-19 17:55:31 +0100

    fix(cli): don't force native-tls feature on desktop

--- crates/tauri-cli/src/interface/rust.rs
+++ crates/tauri-cli/src/interface/rust.rs
@@ -365,27 +365,14 @@ fn shared_options(
   mobile: bool,
   args: &mut Vec<String>,
   features: &mut Option<Vec<String>>,
-  app_settings: &RustAppSettings,
 ) {
   if mobile {
     args.push("--lib".into());
     features
       .get_or_insert(Vec::new())
       .push("tauri/rustls-tls".into());
-  } else {
-    if !desktop_dev {
-      args.push("--bins".into());
-    }
-    let all_features = app_settings
-      .manifest
-      .lock()
-      .unwrap()
-      .all_enabled_features(if let Some(f) = features { f } else { &[] });
-    if !all_features.contains(&"tauri/rustls-tls".into()) {
-      features
-        .get_or_insert(Vec::new())
-        .push("tauri/native-tls".into());
-    }
+  } else if !desktop_dev {
+    args.push("--bins".into());
   }
 }
 
@@ -409,7 +396,7 @@ fn dev_options(
   }
   *args = dev_args;
 
-  shared_options(true, mobile, args, features, app_settings);
+  shared_options(true, mobile, args, features);
 
   if !args.contains(&"--no-default-features".into()) {
     let manifest_features = app_settings.manifest.lock().unwrap().features();
@@ -489,7 +476,7 @@ impl Rust {
     features
       .get_or_insert(Vec::new())
       .push("tauri/custom-protocol".into());
-    shared_options(false, mobile, args, features, &self.app_settings);
+    shared_options(false, mobile, args, features);
   }
 
   fn run_dev<F: Fn(Option<i32>, ExitReason) + Send + Sync + 'static>(
